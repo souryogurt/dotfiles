d8db5f72a20d29b863ed8b7b891bdaa8c9c514de
Egor Artemov <egor.artemov@gmail.com>
2023-02-16 18:16:18 +0800

    Enable Treesitter and telescope if present

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,3 +28,25 @@ if pcall(vim.cmd.packadd, "catppuccin") then
     require("catppuccin").setup({ term_colors = true })
     vim.cmd.colorscheme("catppuccin-mocha")
 end
+
+if pcall(vim.cmd.packadd, "nvim-treesitter") then
+    require("nvim-treesitter.configs").setup({
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        incremental_selection = { enable = true },
+        indent = { enable = true },
+    })
+end
+
+if pcall(vim.cmd.packadd, "plenary") then
+    pcall(vim.cmd.packadd, "nvim-web-devicons")
+    if pcall(vim.cmd.packadd, "telescope") then
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    end
+end
