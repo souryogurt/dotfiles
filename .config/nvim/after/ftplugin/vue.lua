4f855e33972c3c9b924cc0733ded963b6ea13836
Egor Artemov <egor.artemov@gmail.com>
2023-03-30 03:41:17 +0800

    Use 2 spaces for indenting Vue files

--- .config/nvim/after/ftplugin/vue.lua
+++ .config/nvim/after/ftplugin/vue.lua
@@ -1,5 +1,5 @@
-vim.opt_local.shiftwidth = 4
-vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
 vim.opt_local.expandtab = true
 vim.opt_local.wrap = false
 vim.opt_local.number = true
