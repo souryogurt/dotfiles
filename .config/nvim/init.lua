a21817997168c45e20e7fdb614d832e45164792d
Egor Artemov <egor.artemov@gmail.com>
2023-04-16 14:35:41 +0800

    Use luasnip for completion in neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -62,6 +62,7 @@ if pcall(vim.cmd.packadd, "plenary") then
     end
 end
 
+--[[
 if pcall(vim.cmd.packadd, "lsp-zero") then
     pcall(vim.cmd.packadd, "nvim-lspconfig") -- +
     pcall(vim.cmd.packadd, "mason") -- +
@@ -71,9 +72,8 @@ if pcall(vim.cmd.packadd, "lsp-zero") then
     pcall(vim.cmd.packadd, "cmp-nvim-lsp") -- +
     pcall(vim.cmd.packadd, "cmp-buffer")
     pcall(vim.cmd.packadd, "cmp-path")
-    pcall(vim.cmd.packadd, "LuaSnip")
+    pcall(vim.cmd.packadd, "LuaSnip") -- +
     pcall(vim.cmd.packadd, "cmp-nvim-lua")
-
     pcall(vim.cmd.packadd, "null-ls")
 
     local lsp = require("lsp-zero").preset({
@@ -107,138 +107,134 @@ if pcall(vim.cmd.packadd, "lsp-zero") then
         },
     })
 end
+--]]
 
+local lspconfig
+local lsp_capabilities = {}
 if pcall(vim.cmd.packadd, "nvim-lspconfig") then
-    local lspconfig = require("lspconfig")
+    lspconfig = require("lspconfig")
+end
+
+if pcall(vim.cmd.packadd, "LuaSnip") then
     if pcall(vim.cmd.packadd, "nvim-cmp") then
+        local luasnip = require("luasnip")
         local cmp = require("cmp")
         local sources = {}
-        if pcall(vim.cmd.packadd, "cmp-nvim-lsp") then
+
+        -- setup sources
+        if lspconfig and pcall(vim.cmd.packadd, "cmp-nvim-lsp") then
             table.insert(sources, { name = "nvim_lsp" })
-            local function lsp_attach(client, bufnr)
-                -- TODO: Create your keybindings here
-                local nmap = function(keys, func, desc)
-                    if desc then
-                        desc = "LSP: " .. desc
-                    end
-                    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-                end
-
-                nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-                nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-                nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-                nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-                nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-                nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-                nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-                nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-                -- See `:help K` for why this keymap
-                nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-                nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-                -- Lesser used LSP functionality
-                nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-                nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-                nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-                nmap("<leader>wl", function()
-                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, "[W]orkspace [L]ist Folders")
-
-                -- Create a command `:Format` local to the LSP buffer
-                vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-                    vim.lsp.buf.format()
-                end, { desc = "Format current buffer with LSP" })
-            end
-            local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-            if pcall(vim.cmd.packadd, "mason") then
-                require("mason").setup()
-                if pcall(vim.cmd.packadd, "mason-lspconfig") then
-                    local mason_lspconfig = require("mason-lspconfig")
-                    mason_lspconfig.setup()
-                    mason_lspconfig.setup_handlers({
-                        function(server_name)
-                            lspconfig[server_name].setup({
-                                on_attach = lsp_attach,
-                                capabilities = lsp_capabilities,
-                            })
-                        end,
-                    })
-                end
-            else
-                -- setup lspconfig manually
-            end
+            local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+            lsp_capabilities = vim.tbl_deep_extend("force", lsp_capabilities, cmp_capabilities)
         end
 
+        -- setup cmp
         local has_words_before = function()
             unpack = unpack or table.unpack
             local line, col = unpack(vim.api.nvim_win_get_cursor(0))
             return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
         end
-
+        local border_ops = {
+            border = "single",
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+        }
         cmp.setup({
-            sources = sources,
-            mapping = cmp.mapping.preset.insert({
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                --["<C-Space>"] = cmp.mapping.complete({}),
-                ["<CR>"] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                }),
-
-                ["<C-Space>"] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Insert,
-                    select = true,
-                }),
-
-                ["<Tab>"] = function(fallback)
-                    if not cmp.select_next_item() then
-                        if vim.bo.buftype ~= "prompt" and has_words_before() then
-                            cmp.complete()
-                        else
-                            fallback()
-                        end
-                    end
-                end,
-
-                ["<S-Tab>"] = function(fallback)
-                    if not cmp.select_prev_item() then
-                        if vim.bo.buftype ~= "prompt" and has_words_before() then
-                            cmp.complete()
-                        else
-                            fallback()
-                        end
-                    end
-                end,
-            }),
             snippet = {
-                -- We recommend using *actual* snippet engine.
-                -- It's a simple implementation so it might not work in some of the cases.
                 expand = function(args)
-                    unpack = unpack or table.unpack
-                    local line_num, col = unpack(vim.api.nvim_win_get_cursor(0))
-                    local line_text = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, true)[1]
-                    local indent = string.match(line_text, "^%s*")
-                    local replace = vim.split(args.body, "\n", true)
-                    local surround = string.match(line_text, "%S.*") or ""
-                    local surround_end = surround:sub(col)
-
-                    replace[1] = surround:sub(0, col - 1) .. replace[1]
-                    replace[#replace] = replace[#replace] .. (#surround_end > 1 and " " or "") .. surround_end
-                    if indent ~= "" then
-                        for i, line in ipairs(replace) do
-                            replace[i] = indent .. line
-                        end
-                    end
-
-                    vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, replace)
+                    luasnip.lsp_expand(args.body)
                 end,
             },
-        }, {
-            { name = "buffer" },
+            window = {
+                completion = cmp.config.window.bordered(border_ops),
+                documentation = cmp.config.window.bordered(border_ops),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-e>"] = cmp.mapping.abort(),
+
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                    -- they way you will only jump inside the snippet region
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
+                ["<CR>"] = cmp.mapping.confirm({ select = false }),
+            }),
+            sources = cmp.config.sources(sources),
         })
+    end
+end
+
+if lspconfig then
+    local function lsp_attach(_, bufnr)
+        local nmap = function(keys, func, desc)
+            if desc then
+                desc = "LSP: " .. desc
+            end
+            vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+        end
+
+        nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+        nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+        nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+        nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+        nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+        nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+        nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+        nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+        -- See `:help K` for why this keymap
+        nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+        nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+        -- Lesser used LSP functionality
+        nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+        nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+        nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+        nmap("<leader>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, "[W]orkspace [L]ist Folders")
+        -- Create a command `:Format` local to the LSP buffer
+        vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+            vim.lsp.buf.format()
+        end, { desc = "Format current buffer with LSP" })
+    end
+    if pcall(vim.cmd.packadd, "mason") then
+        require("mason").setup()
+        if pcall(vim.cmd.packadd, "mason-lspconfig") then
+            local mason_lspconfig = require("mason-lspconfig")
+            mason_lspconfig.setup()
+            mason_lspconfig.setup_handlers({
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        on_attach = lsp_attach,
+                        capabilities = lsp_capabilities,
+                    })
+                end,
+            })
+        end
     else
+        -- setup lspconfig manually
+        -- lspconfig["tsserver"].setup({
+        --     on_attach = lsp_attach,
+        --     capabilities = lsp_capabilities,
+        -- })
     end
 end
