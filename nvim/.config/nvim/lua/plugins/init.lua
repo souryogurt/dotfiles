4837ece977cd759099eb1ae9d1689a68b4168471
Egor Artemov <egor.artemov@gmail.com>
2023-09-24 02:36:50 +0800

    Fix chatgpt neovim colors

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -13,6 +13,17 @@ return {
                     neotree = true,
                     which_key = true,
                 },
+                custom_highlights = function(colors)
+                    return {
+                        ChatGPTQuestion = { fg = colors.saphire },
+                        ChatGPTWelcome = { fg = colors.overlay1 },
+                        ChatGPTTotalTokens = { fg = colors.crust, bg = colors.blue },
+                        ChatGPTTotalTokensBorder = { fg = colors.blue },
+                        ChatGPTMessageAction = { fg = colors.crust, bg = colors.blue },
+                        ChatGPTCompletion = { fg = colors.text },
+                        FloatBorder = { bg = colors.base }
+                    }
+                end
             })
             vim.cmd.colorscheme("catppuccin-mocha")
         end,
