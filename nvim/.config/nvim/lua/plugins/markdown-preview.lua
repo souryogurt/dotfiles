4460086a2e5686580a1f0a825b2df0ae709f2c27
Egor Artemov <egor.artemov@gmail.com>
2023-10-06 03:40:13 +0800

    Enable the dark theme for the markdown preview

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -3,4 +3,7 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
+    init = function()
+        vim.g.mkdp_theme = 'dark'
+    end
 }
