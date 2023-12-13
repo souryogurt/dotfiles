a4dd2690bc4a357ef8e0097ec4bab1f65fd08e7a
Egor Artemov <egor.artemov@gmail.com>
2023-12-14 05:38:29 +0800

    Add go.nvim and move plugins

---
+++ nvim/.config/nvim/lua/plugins/go-nvim.lua
@@ -0,0 +1,33 @@
+return {
+  "ray-x/go.nvim",
+  dependencies = {  -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
+    local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local lsp_capabilities = vim.tbl_deep_extend("force", lspconfig.util.default_config, cmp_capabilities)
+    require("go").setup({
+        lsp_cfg = {
+            capabilities = lsp_capabilities,
+        },
+        diagnostic = false,
+        lsp_inlay_hints = {
+            enable = false,
+        },
+    })
+    local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+            require('go.format').goimport()
+        end,
+        group = format_sync_grp,
+    })
+  end,
+  event = {"CmdlineEnter"},
+  ft = {"go", 'gomod'},
+  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+}
--- nvim/.config/nvim/lua/plugins/vim-go.lua
+++ nvim/.config/nvim/lua/plugins/vim-go.lua
@@ -2,6 +2,8 @@ local plugin = { "fatih/vim-go" }
 
 plugin.ft = "go"
 
+plugin.enabled = false
+
 function plugin.config()
     vim.g.go_fmt_command = "goimports"
 end
